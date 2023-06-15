d64ea060788915096fb8ff94decf151922db7c61
inarvekar <inarvekar@qualys.com>
2023-06-15 23:43:52 +0530

    Solved Problem 69

---
+++ src/Easy/69Sqrt.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/sqrtx/description/
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        first, last = 1, x
+        while first <= last:
+            mid = first + (last - first) // 2
+            if mid == x // mid:
+                return mid
+            elif mid > x // mid:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return last
+
+
+obj = Solution()
+print(obj.mySqrt(169))  # 3
+print(obj.mySqrt(216))   # 58
+print(obj.mySqrt(625))   #1994
